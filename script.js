a80d40ca5b9c95ebc34e3146c6740d4a06439218
Jhoan Nicolas Cuervo Lozada <44857886+JhoanCuervo@users.noreply.github.com>
2026-06-05 12:57:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-let deferredPrompt;
-const btnInstalar = document.getElementById("btn-instalar");
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  btnInstalar.style.display = "block";
-});
-
-btnInstalar.addEventListener("click", async () => {
-  if (!deferredPrompt) return;
-  deferredPrompt.prompt();
-  const { outcome } = await deferredPrompt.userChoice;
-  console.log("Resultado instalación:", outcome);
-  deferredPrompt = null;
-  btnInstalar.style.display = "none";
-});
-
-window.addEventListener("appinstalled", () => {
-  console.log("PWA instalada");
-  btnInstalar.style.display = "none";
-});
-
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("./sw.js")
