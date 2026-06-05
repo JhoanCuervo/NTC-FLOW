c51b28ba42bfa1cc9b6cbe5de879f03bd5f1981a
Jhoan Nicolas Cuervo Lozada <44857886+JhoanCuervo@users.noreply.github.com>
2026-06-05 12:38:11 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# pwa-react-apps-script
+
+Simplemente hay que clonar la aplicación
+cambiar la ruta de apps script y algunos de los datos del manifest como las rutas de los iconos
---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "v1_cache_panel_adm",
+  urlsToCache = [
+    "https://script.google.com/macros/s/AKfycbzNIAxoA6gU9efPWsbmWDz9zVDA78pr_65vPU4a-Dz4IlqnT6FPFSK2KoJwOvQU-Ie9/exec",
+    "./manifest.json",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => "fallo el registro del cache ", err)
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const caheWitheList = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys().then((cachesNames) =>
+      cachesNames.map((cacheName) => {
+        if (cacheName.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
